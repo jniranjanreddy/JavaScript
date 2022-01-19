77fe8beb15e38f00bc814e32960e742105cf9b88
Niranjan <niranjangitlab@gmail.com>
2022-01-19 21:44:02 +0530

    l

---
+++ fundamentals-1/script.js
@@ -0,0 +1,15 @@
+/*
+let js = 'amazing';
+//if (js === 'amazing') alert('Java Script is FUN!!');
+let firstName  = "Niranjan";
+let secondName = "Reddy";
+let lastName   = "Jannapureddy";
+let age = 30;
+
+console.log(typeof firstName)
+console.log(typeof age)
+console.log(`${firstName} ${secondName} ${lastName}`)
+
+true;
+console.log(true)
+*/
